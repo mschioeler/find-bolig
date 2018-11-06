5b15518b7b6b93750b3970d7a244b4a5bc53658a
Morten Emmanuel Schiøler <mschioeler@deloitte.dk>
2018-11-06 20:26:54 +0100

    Alpha version

--- src/main/java/dk/mschioeler/Main.java
+++ src/main/java/dk/mschioeler/Main.java
@@ -1,14 +1,13 @@
 package dk.mschioeler;
 
-import com.sun.istack.internal.Nullable;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 
+import javax.annotation.Nullable;
+import java.time.Duration;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
@@ -19,28 +18,27 @@ public class Main {
     private static final By USERNAME = By.xpath(".//input[contains(@id, 'UserName')]");
     private static final By PASSWORD = By.xpath(".//input[contains(@id, 'Password')]");
     private static final By LOGIN_BUTTON = By.xpath("//div[@class='formSectionSubmit']//a[text()='Log ind']");
-    private static final String SEARCH_QUERY = "https://www.findbolig.nu/ledigeboliger/liste.aspx?&adr=Myggen%C3%A6sgade&showrented=1&showyouth=1&showlimitedperiod=1&showunlimitedperiod=1&showOpenDay=0&focus=ctl00_placeholdersidebar_0_txt_Address";
-    private static final String SEARCH_QUERY_MOCK = "https://www.findbolig.nu/ledigeboliger/liste.aspx?&showrented=1&showyouth=1&showlimitedperiod=1&showunlimitedperiod=1&showOpenDay=0&focus=ctl00_placeholdersidebar_0_txt_Address";
+    private static final String DEFAULT_SEARCH_QUERY = "https://www.findbolig.nu/ledigeboliger/liste.aspx?&adr=Myggen%C3%A6sgade&showrented=1&showyouth=1&showlimitedperiod=1&showunlimitedperiod=1&showOpenDay=0&focus=ctl00_placeholdersidebar_0_txt_Address";
     private static final By ANSOEG_BOLIG = By.xpath("//a[text()='Ansøg bolig']");
 
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\W92262\\Downloads\\chromedriver_win32\\chromedriver.exe");
         String driverType = args[0];
         String user = args[1];
         String password = args[2];
+        final String query = args.length > 3? args[3]: DEFAULT_SEARCH_QUERY;
         final WebDriver driver = DriverFactory.getDriver(driverType);
         driver.get("https://www.findbolig.nu/");
         login(driver, user, password);
 
-        driver.get(SEARCH_QUERY);
+        driver.get(query);
 
         FluentWait<WebDriver> wait = new FluentWait<>(driver)
-                .pollingEvery(10, TimeUnit.SECONDS)
-                .withTimeout(2, TimeUnit.DAYS);
+                .pollingEvery(Duration.ofSeconds(5))
+                .withTimeout(Duration.ofDays(2));
         WebElement hit = wait.until(new Function<WebDriver, WebElement>() {
             @Override
             public WebElement apply(@Nullable WebDriver input) {
-                driver.get(SEARCH_QUERY_MOCK);
+                driver.get(query);
                 List<WebElement> results = driver.findElements(By.xpath("//*[@id='GridView_Results']//tr[@aid]"));
                 if (results.size() == 0) return null;
                 if (results.size() == 1) {
